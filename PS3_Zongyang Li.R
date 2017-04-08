80ee8c0efb5772025f30a98bd3d00f6d7539c1a1
elmerzyli <elmerleezy@gmail.com>
2017-04-08 17:45:56 -0400

    PS3: Try markdown, Improve answer

--- PS3_Zongyang Li.R
+++ PS3_Zongyang Li.R
@@ -71,12 +71,11 @@ stargazer(weo.probit_3,type="text")
 ## Comment: 
 	# From the above two regression results, we can see that excluding six month libor rate and Output gap 
 	# in percent of potential GDP has significantly improve the explanatory power of the model. 
-	# This can be seen from the increased p-values of other variables once excluding these two, and the increased 
-	# AIC, from 4 to 14 to 32. This might because that these two vairables have too many missing values
+	# This might because that these two vairables have too many missing values
 	# We then try to exclude change in volume of exports of goods and services, which is the least significant from last regression: 
 	weo.probit_4 <- glm(gen_str_bal_2 ~ gdp_growth + gen_lend_borrow_net + gen_gov_rev + gns, family = binomial(link = "probit"), data = weo_3)
 	summary(weo.probit_4)
-	# but then we can see that the AIC is 30 now,lower than before.So we may conclude that there are no variables we want to exclude for now
+	# but then we can see that the AIC is 30 now,lower than before which is 32. So the model is performing better. 
 
 	# For variables that we want to include, since our dependent variable is general government structural balance
 	# since we only have export, we may consider import as well. Also we may want to include savings cause that might have 
@@ -155,20 +154,11 @@ table(train$crime_compare, crime.qda_cv$class, dnn = c('Actual Group','Predicted
 
 ## Compare - Method 2: manul cross validation and compare MSE
 
-# build train and test datasets
-	# index <- sample(1:nrow(crime_1),round(0.75*nrow(crime_1))) 
-	# train <- crime_1[index,]
-	# test <- crime_1[-index,]
-# fit model
-	# crime.lda_2 <- lda(crime_compare ~ clrprc1+clrprc2+d78, data=train, na.action="na.omit")
-	# crime.qda_2 <- qda(crime_compare ~ clrprc1+clrprc2+d78, data=train, na.action="na.omit")
 # predict on test dataset
-		# pr.lda <- predict(crime.lda_2,test[,4:6])
-		# pr.qda <- predict(crime.qda_2,test[,4:6])
 	pr.qda <- predict(crime.qda,newdata = test)
 	# turn result to integer
-	pr.lda_class <- as.integer(pr.lda$class)
-	pr.qda_class <- as.integer(pr.qda$class)
+	pr.lda_class <- as.integer(as.character(pr.lda$class))
+	pr.qda_class <- as.integer(as.character(pr.qda$class))
 # caculate MSE
 MSE.lda <- sum((test$crime_compare - pr.lda_class)^2)/nrow(test)
 MSE.qda <- sum((test$crime_compare - pr.qda_class)^2)/nrow(test)
@@ -176,7 +166,7 @@ MSE.qda <- sum((test$crime_compare - pr.qda_class)^2)/nrow(test)
 print(paste(MSE.lda,MSE.qda))
 
 ## Comment: 
-	# From the result of MSE - 1.21 for LDA and 1.11 for QDA we may conclude that QDA performs better
+	# From the result of MSE: 0.25 for LDA and 0.19 for QDA we may conclude that QDA performs better
 
 
 #####################
@@ -192,7 +182,7 @@ knntest <- test[,c(4:5,14)]
 # fit model, predict and see MSE
 pr.knn <- knn(knntrain,knntest,knntrain$crime_compare, k = 1)
 table(pr.knn,knntest$crime_compare)
-pr.knn_class <- as.integer(pr.knn)
+pr.knn_class <- as.integer(as.character(pr.knn))
 MSE.knn_0 <- sum((knntest$crime_compare - pr.knn_class)^2)/nrow(knntest)
 MSE.knn_0
 
@@ -201,20 +191,20 @@ MSE.knn <- rep(0, 20)
 k <- 1:20
 for(x in k){
 	pr.knn <- knn(knntrain,knntest,knntrain$crime_compare, k = x)
-	pr.knn_class <- as.integer(pr.knn)
+	pr.knn_class <- as.integer(as.character(pr.knn))
 	MSE.knn[x] <- sum((knntest$crime_compare - pr.knn_class)^2)/nrow(knntest)
 }
 plot(k, MSE.knn, type = 'b')
-## Comment: From the plotted graph we can see that the MSE is minimized at k=9, so we choose k=9 in this case
+## Comment: From the plotted graph we can see that the MSE is minimized at k=5, so we choose k=5 in this case
 
-pr.knn <- knn(knntrain,knntest,knntrain$crime_compare, k = 9)
+pr.knn <- knn(knntrain,knntest,knntrain$crime_compare, k = 5)
 table(pr.knn,knntest$crime_compare)
-pr.knn_class <- as.integer(pr.knn)
+pr.knn_class <- as.integer(as.character(pr.knn))
 MSE.knn <- sum((knntest$crime_compare - pr.knn_class)^2)/nrow(knntest)
 MSE.knn
 
 print(paste(MSE.lda,MSE.qda,MSE.knn))
-## Comment: we can see that the MSE for kNN with k set at 1 is 1.19, which less than LDA but more than QDA
+## Comment: we can see that the MSE for kNN with k set at 5 is 0.17, which less than both LDA and QDA
 
 
 ##################################################
@@ -226,7 +216,7 @@ smoke <- read.table("smoke.txt")
 smoke_1 <- smoke %>%
 	dplyr::mutate(faminc = V1, cigtax = V2, cigprice = V3, bwght = V4, fatheduc = V5, motheduc = V6, parity = V7, male = V8, white = V9, cigs = V10, lbwght = V11, bwghtlbs = V12, packs = V13, lfaminc = V14) %>%
 	dplyr::select(faminc, cigtax, cigprice, bwght, fatheduc, motheduc, parity, male, white, cigs, lbwght, bwghtlbs, packs, lfaminc) %>%
-	dplyr::mutate(smokes = ifelse(cigs>0,1,0), motheduc = as.integer(motheduc))
+	dplyr::mutate(smokes = ifelse(cigs>0,1,0), motheduc = as.integer(as.character(motheduc)))
 
 #####################
 ## Problem 5.1
@@ -239,7 +229,7 @@ smoke_16 <- smoke_1 %>% mutate(lfaminc = mean(lfaminc, na.rm = T), motheduc = 16
 smoke_12 <- smoke_1 %>% mutate(lfaminc = mean(lfaminc, na.rm = T), motheduc = 12)
 # predict the dependent
 p_edu16 <- predict(smoke_preg.probit,smoke_16)
-p_edu12 <- predict(smoke_preg.probit,smoke_16)
+p_edu12 <- predict(smoke_preg.probit,smoke_12)
 
 mean(p_edu16 - p_edu12) # result: -0.05189609
 
@@ -254,7 +244,7 @@ summary(smoke_preg.probit)
 
 ## Comment: 
 	# faminc is not very likely to be exogeneous in this equation since there are factors that impact both income level 
-	# and smoking, such as previous health habits(eating habits etc.) which will influcence both one's income and possibility to smoke
+	# and smoking, such as health habits(eating habits etc.) which will influcence both one's income and possibility to smoke
 
 	# motheredu isn't likely to be exogeneous since education is so correlated to income and there is likely to be other
 	# unobservables that might influence both motheredu and smoke
@@ -279,5 +269,5 @@ h <- hausman.systemfit( fit2sls, fit3sls )
 print( h )
 
 ## Comment: 
-	# from the p-value of the Huasman test statistic - 0.78 is higher than any standard, we fail reject the null hypothesis that log-faminc in exogeneous
+	# from the p-value of the Huasman test statistic : 0.78 is higher than any standard, we fail reject the null hypothesis that log-faminc in exogeneous
 
